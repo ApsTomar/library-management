7f118f6a4958325c56b7e47469def67317a8f49d
abhay <abhaypratap305@gmail.com>
2019-09-24 15:14:10 +0530

    get books handlers added

--- cmd/book-svc/handlers.go
+++ cmd/book-svc/handlers.go
@@ -10,6 +10,8 @@ import (
 	"github.com/library/models"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func GetAuthInfoFromContext(ctx context.Context) *models.AuthInfo {
@@ -21,15 +23,21 @@ func addAuthor(w http.ResponseWriter, r *http.Request) {
 	if authInfo.Role != models.AdminAccount {
 		glog.Errorf("permission denied")
 		http.Error(w, "Only Admin is authorized to add authors", http.StatusUnauthorized)
+		return
 	}
 	author := &models.Author{}
 	err := json.NewDecoder(r.Body).Decode(author)
 	if err != nil {
 		glog.Errorf("error while decoding request body: %v", err)
 		http.Error(w, "error while decoding request body", http.StatusInternalServerError)
+		return
 	}
 	err = dataStore.CreateAuthor(*author)
 	if err != nil {
+		if strings.Contains(err.Error(), "1062") {
+			glog.Errorf("duplicate entry: %v", err)
+			http.Error(w, "duplicate entry", http.StatusBadRequest)
+		}
 		glog.Errorf("error creating new author: %v", err)
 		http.Error(w, "error creating new author", http.StatusInternalServerError)
 	}
@@ -39,16 +47,24 @@ func addBook(w http.ResponseWriter, r *http.Request) {
 	authInfo := GetAuthInfoFromContext(r.Context())
 	if authInfo.Role != models.AdminAccount {
 		glog.Errorf("permission denied")
-		http.Error(w, "Only Admin is authorized to add authors", http.StatusUnauthorized)
+		http.Error(w, "Only Admin is authorized to add books", http.StatusUnauthorized)
+		return
 	}
 	book := &models.Book{}
 	err := json.NewDecoder(r.Body).Decode(book)
 	if err != nil {
 		glog.Errorf("error while decoding request body: %v", err)
 		http.Error(w, "error while decoding request body", http.StatusInternalServerError)
+		return
 	}
+	book.AvailableDate = time.Now()
+	book.Available = true
 	err = dataStore.CreateBook(*book)
 	if err != nil {
+		if strings.Contains(err.Error(), "1062") {
+			glog.Errorf("duplicate entry: %v", err)
+			http.Error(w, "duplicate entry", http.StatusBadRequest)
+		}
 		glog.Errorf("error creating new book: %v", err)
 		http.Error(w, "error creating new book", http.StatusInternalServerError)
 	}
@@ -58,16 +74,22 @@ func addSubject(w http.ResponseWriter, r *http.Request) {
 	authInfo := GetAuthInfoFromContext(r.Context())
 	if authInfo.Role != models.AdminAccount {
 		glog.Errorf("permission denied")
-		http.Error(w, "Only Admin is authorized to add authors", http.StatusUnauthorized)
+		http.Error(w, "Only Admin is authorized to add subjects", http.StatusUnauthorized)
+		return
 	}
 	subject := &models.Subject{}
 	err := json.NewDecoder(r.Body).Decode(subject)
 	if err != nil {
 		glog.Errorf("error while decoding request body: %v", err)
 		http.Error(w, "error while decoding request body", http.StatusInternalServerError)
+		return
 	}
 	err = dataStore.CreateSubject(*subject)
 	if err != nil {
+		if strings.Contains(err.Error(), "1062") {
+			glog.Errorf("duplicate entry: %v", err)
+			http.Error(w, "duplicate entry", http.StatusBadRequest)
+		}
 		glog.Errorf("error creating new subject: %v", err)
 		http.Error(w, "error creating new subject", http.StatusInternalServerError)
 	}
@@ -78,6 +100,22 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		glog.Errorf("error fetching books: %v", err)
 		http.Error(w, "error fetching books", http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(books)
+	if err != nil {
+		glog.Errorf("error encoding json response: %v", err)
+		http.Error(w, "error encoding json response", http.StatusInternalServerError)
+	}
+}
+
+func getBooksByName(w http.ResponseWriter, r *http.Request) {
+	name := chi.URLParam(r, "name")
+	books, err := dataStore.GetBooksByName(name)
+	if err != nil {
+		glog.Errorf("error fetching books: %v", err)
+		http.Error(w, "error fetching books", http.StatusInternalServerError)
+		return
 	}
 	err = json.NewEncoder(w).Encode(books)
 	if err != nil {
@@ -91,11 +129,14 @@ func getBookByBookID(w http.ResponseWriter, r *http.Request) {
 	bookID, err := strconv.Atoi(id)
 	if err != nil {
 		glog.Errorf("error parsing bookID: %v", err)
+		http.Error(w, "error parsing bookID", http.StatusInternalServerError)
+		return
 	}
-	books, err := dataStore.GetBooksByID(uint(bookID))
+	books, err := dataStore.GetBookByID(uint(bookID))
 	if err != nil {
 		glog.Errorf("error fetching book: %v", err)
 		http.Error(w, "error fetching book", http.StatusInternalServerError)
+		return
 	}
 	err = json.NewEncoder(w).Encode(books)
 	if err != nil {
@@ -109,11 +150,14 @@ func getBooksByAuthorID(w http.ResponseWriter, r *http.Request) {
 	authorID, err := strconv.Atoi(id)
 	if err != nil {
 		glog.Errorf("error parsing authorID: %v", err)
+		http.Error(w, "error parsing authorID", http.StatusInternalServerError)
+		return
 	}
 	books, err := dataStore.GetBooksByAuthor(uint(authorID))
 	if err != nil {
 		glog.Errorf("error fetching books: %v", err)
 		http.Error(w, "error fetching books", http.StatusInternalServerError)
+		return
 	}
 	err = json.NewEncoder(w).Encode(books)
 	if err != nil {
@@ -127,11 +171,14 @@ func getBooksBySubjectID(w http.ResponseWriter, r *http.Request) {
 	subjectID, err := strconv.Atoi(id)
 	if err != nil {
 		glog.Errorf("error parsing subjectID: %v", err)
+		http.Error(w, "error parsing subjectID", http.StatusInternalServerError)
+		return
 	}
 	books, err := dataStore.GetBooksBySubject(uint(subjectID))
 	if err != nil {
 		glog.Errorf("error fetching books: %v", err)
 		http.Error(w, "error fetching books", http.StatusInternalServerError)
+		return
 	}
 	err = json.NewEncoder(w).Encode(books)
 	if err != nil {
@@ -150,6 +197,7 @@ func getSubjects(w http.ResponseWriter, r *http.Request) {
 			glog.Errorf("error fetching all subjects: %v", err)
 			http.Error(w, "error fetching all subjects", http.StatusInternalServerError)
 		}
+		return
 	}
 	err = json.NewEncoder(w).Encode(subjects)
 	if err != nil {
@@ -168,8 +216,45 @@ func getAuthors(w http.ResponseWriter, r *http.Request) {
 			glog.Errorf("error fetching all authors: %v", err)
 			http.Error(w, "error fetching all authors", http.StatusInternalServerError)
 		}
+		return
 	}
 	err = json.NewEncoder(w).Encode(authors)
+	if err != nil {
+		glog.Errorf("error encoding json response: %v", err)
+		http.Error(w, "error encoding json response", http.StatusInternalServerError)
+	}
+}
+
+func getAuthorByName(w http.ResponseWriter, r *http.Request) {
+	name := chi.URLParam(r, "name")
+	authors, err := dataStore.GetAuthorsByName(name)
+	if err != nil {
+		glog.Errorf("error fetching authors by name: %v", err)
+		http.Error(w, "error fetching authors by name", http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(authors)
+	if err != nil {
+		glog.Errorf("error encoding json response: %v", err)
+		http.Error(w, "error encoding json response", http.StatusInternalServerError)
+	}
+}
+
+func getAuthorByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	authorID, err := strconv.Atoi(id)
+	if err != nil {
+		glog.Errorf("error parsing authorID: %v", err)
+		http.Error(w, "error parsing authorID", http.StatusInternalServerError)
+		return
+	}
+	author, err := dataStore.GetAuthorByID(uint(authorID))
+	if err != nil {
+		glog.Errorf("error fetching author by id: %v", err)
+		http.Error(w, "error fetching author by id", http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(author)
 	if err != nil {
 		glog.Errorf("error encoding json response: %v", err)
 		http.Error(w, "error encoding json response", http.StatusInternalServerError)
